d07428fc15122b88804daf4a0a295d4d72d1eda1
shreydan <iamshreydan@gmail.com>
2021-01-21 21:55:31 +0530

    click handler added - TODO CardHandler.js

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,19 @@
 import './Card.css';
+import React, { useContext } from 'react';
+import {GameContext} from '../Contexts/GameContext';
 
 
 function Card(props) {
     
-    
+    const {_,__,active} = useContext(GameContext);
+    const [activeCard, setActiveCard] = active;
 
     var cardchars = props.text.split('');
     console.log(cardchars);
     return(
-    <div className="card" style={{"backgroundColor": props.color, "zIndex": props.zIndex}}>
+    <div className="card"
+        style={{"backgroundColor": props.color, "zIndex": props.zIndex}}
+        onClick={() => setActiveCard({"value": props.text, "color": props.color})}>
         {
             cardchars.map((letter) => {
             return <div>{letter}</div>
--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,13 +1,19 @@
 import './Player.css'
 import Card from '../Components/Card';
 
+
 function Player(props) {
     var cards = props.cards;
+    
     return (
         <div className="player">
             {
                 cards.map((card, key)=>{
-                    return <Card text={card.value} color={card.color} zIndex={key}/>
+                    return <Card text={card.value} 
+                                 color={card.color} 
+                                 zIndex={key}
+                                 onclick={console.log(card.value,card.color)}
+                            />
                   })
             }
         </div>
