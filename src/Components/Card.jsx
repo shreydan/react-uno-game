27990c3e3ad63d56dcd690338b8b1c50375b7285
shreydan <iamshreydan@gmail.com>
2021-02-02 13:53:06 +0530

    moved things around, game: unfinished

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,29 +1,18 @@
 import './Card.css';
-import React, { useContext } from 'react';
-import {GameContext} from '../Contexts/GameContext';
-import {checkPlayable} from '../cardHandler';
 
 function Card(props) {
     
-    const {_,__,active} = useContext(GameContext);
-
-
-    var playable = checkPlayable(active, [props.text, props.color]);
 
     var cardchars = props.text.split('');
-    console.log(cardchars);
     return(
-    <div className="card"
-        style={{"backgroundColor": props.color, "zIndex": props.zIndex}}
-        onClick={()=>  playable ? 
-                        active.setActiveCard({ "value": props.text, "color": props.color })
-                         : console.log("unplayable")}>
-        {
-            cardchars.map((letter) => {
-            return <div>{letter}</div>
-              })
-        }
-    </div>
+        <div className="card"
+            style={{"backgroundColor": props.color, "zIndex": props.zIndex}}>
+            {
+                cardchars.map((letter) => {
+                return <div>{letter}</div>
+                })
+            }
+        </div>
     );
 
 }
--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,10 +1,12 @@
+import React, { useContext } from 'react';
+import {GameContext} from '../Contexts/GameContext';
 import './Player.css'
 import Card from '../Components/Card';
-
+import {checkPlayable} from '../cardHandler';
 
 function Player(props) {
     var cards = props.cards;
-    
+    const {active} = useContext(GameContext);
     return (
         <div className="player">
             {
