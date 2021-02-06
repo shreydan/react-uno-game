4c403613965c20ae6086efabbb04f225eb033b09
shreydan <iamshreydan@gmail.com>
2021-02-06 22:38:24 +0530

    added card handler for wild cards

--- src/cardHandler.js
+++ src/cardHandler.js
@@ -1,8 +1,10 @@
 export const checkPlayable = (activeCard, currentCard) => {
     console.log(activeCard[0], currentCard)
     var clickable = false;
+    if(currentCard.color === 'black') {
+        clickable = true;
+    }
     if(activeCard[0].value === currentCard.value || activeCard[0].color === currentCard.color) {
-
         clickable = true;
     }
 
